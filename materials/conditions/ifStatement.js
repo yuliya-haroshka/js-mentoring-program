c969b38932f9b73bd75a9949175b5d1f63874183
Dzmitry Shaplyka <Dzmitry_Shaplyka@epam.com>
2020-02-13 10:58:42 +0300

    conditions added

---
+++ materials/conditions/ifStatement.js
@@ -0,0 +1,34 @@
+if (2 * 4 == 8) {
+      console.log('That is right!');
+  } else {
+      console.log('Wrong!');
+}
+
+const num = 50;
+
+if (num < 49) {
+  console.log('Wrong!');
+} else if (num > 100) {
+    console.log('Too much!');
+} else {
+    console.log('That is right!');
+}
+
+//Ternary operator
+(num == 50) ? console.log('That is right!') : console.log('Wrong!');
+
+//Switch / case
+switch (num) {
+  case num < 49:
+    console.log('Wrong!');
+    break;
+  case num > 100:
+    console.log('Too much!');
+    break;
+  case 50:
+    console.log('That is right!');
+    break;
+  default:
+    console.log('Default behaviour'); 
+    break;
+}
