2492440d1214fbdf247a73382e13d4b545375644
Oleksandr Halichenko <oleksandr_halichenko@epam.com>
2020-02-17 16:32:11 +0300

    hw for arrays

--- tasks/arrays/arrays.js
+++ tasks/arrays/arrays.js
@@ -1,31 +1,37 @@
-// return an array of characters names
+/**
+ * return an array of characters names
+ */
 function getCharactersNames(chars) {
-
 }
 
-// print ids of all characters
+/**
+ * print (console.log) ids of all characters
+ */
 function printCharacterNames(chars) {
-
 }
 
-// return an array of non-human characters
+/**
+ * return an array of non-human characters
+ */
 function getNonHumanCharacters(chars) {
-
 }
 
-// return info about character named 'Jerry Smith'
+/**
+ * return info about character named 'Jerry Smith'
+ */
 function getJerryInfo(chars) {
-
 }
 
-// check if all characters are human. return true if all, false if not
+/**
+ * check if all characters are human. return true if all, false if not
+ */
 function isAllHuman(chars) {
-
 }
 
-// check if there are any Fish-Person characters. return true if any, false if not
+/**
+ * check if there are any Fish-Person characters. return true if any, false if not
+ */
 function isAnyFishPerson(chars) {
-
 }
 
 module.exports = {
--- tasks/arrays/test/arrays.spec.js
+++ tasks/arrays/test/arrays.spec.js
@@ -1,4 +1,6 @@
 const { expect } = require('chai');
+const sinon = require('sinon');
+const chars = require('../characters.json');
 const {
     getCharactersNames,
     printCharacterNames,
@@ -9,5 +11,50 @@ const {
 } = require('../arrays');
 
 describe('getCharactersNames', () => {
-    it('should return 3 if passing 1 and 2', () => expect(add(1, 2)).to.equal(3));
+    it('should return character names', () => {
+        const actual = getCharactersNames(chars);
+        const expected = require('./expected_names');
+        expect(expected).to.deep.equal(actual);
+    });
+});
+
+describe('printCharacterNames', () => {
+    it('should print character names', () => {
+        const expected = require('./expected_names');
+        const consoleLogSpy = sinon.spy(console, 'log');
+        printCharacterNames(chars);
+        consoleLogSpy.restore();
+        const calls = consoleLogSpy.getCalls().map(call => call.args[0]);
+        expect(expected).to.deep.equal(calls);
+    });
+});
+
+describe('getNonHumanCharacters', () => {
+    it('should return non-human characters', () => {
+        const actual = getNonHumanCharacters(chars);
+        const expected = require('./expected_non_humans');
+        expect(expected).to.deep.equal(actual);
+    });
+});
+
+describe('getJerryInfo', () => {
+    it('should return jerry info', () => {
+        const actual = getJerryInfo(chars);
+        const expected = require('./jerry');
+        expect(expected).to.deep.equal(actual);
+    });
+});
+
+describe('isAllHuman', () => {
+    it('should false if any non-human', () => {
+        const actual = isAllHuman(chars);
+        expect(actual).to.equal(false);
+    });
+});
+
+describe('isAnyFishPerson', () => {
+    it('should true if any fish person', () => {
+        const actual = isAnyFishPerson(chars);
+        expect(actual).to.equal(true);
+    });
 });
