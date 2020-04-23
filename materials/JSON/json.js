69f189ec6bb45b7dea152e2256a0e57b931b4b63
Dzmitry Shaplyka <Dzmitry_Shaplyka@epam.com>
2020-04-23 16:48:31 +0300

    json js updated

--- materials/JSON/json.js
+++ materials/JSON/json.js
@@ -5,17 +5,19 @@ const options = {
   font: {
       size: '16px',
       color: 'red'
-  }
+  },
+  options: ['red', 'blue'],
+  isChecked: true
 };
 
 /**
  * JSON.stringify method
  */
 const stringifiedJson = JSON.stringify(options);
-//console.log(stringifiedJson);
+console.log(stringifiedJson);
 
 /**
  * JSON.parse method
  */
-const parsed = JSON.parse(optionsJson);
-console.log(parsed);
+const parsed = JSON.parse(stringifiedJson);
+console.log(parsed.name);
