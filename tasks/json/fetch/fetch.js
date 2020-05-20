3e6c364245b425d8e50299c8dc62fc655bd6ff43
Dzmitry Shaplyka <Dzmitry_Shaplyka@epam.com>
2020-05-21 01:08:00 +0300

    fetch task corrected

--- tasks/json/fetch/fetch.js
+++ tasks/json/fetch/fetch.js
@@ -2,6 +2,5 @@
  * 1. You need to make a GET request for the resource: https://jsonplaceholder.typicode.com/posts
  * 2. Save the response to response.json file
  * 3. Save only those items, where id < 20
- * 4. Make the task using async/await
  * DOCS: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
  */
