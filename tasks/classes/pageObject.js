74baae0e5894f61e1507990afe06cc4421028b51
Dzmitry Shaplyka <Dzmitry_Shaplyka@epam.com>
2020-05-21 00:25:08 +0300

    PO task for Classes added

---
+++ tasks/classes/pageObject.js
@@ -0,0 +1,6 @@
+/**
+ * Organize a page object structure of any app you want:
+ * 1. Base page should be created
+ * 2. At least one of the child pages should be inerited from the page
+ * 3. Create a simple walk through scenarion using the described elements (how it was done on the sessions for Login functionality)
+ */
