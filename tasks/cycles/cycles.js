af701e1c4b617e119c9b036eb5526254f9d19c95
Oleksandr_Halichenko <oleksandr_halichenko@epam.com>
2021-01-18 12:09:57 +0300

    task3 rework

--- tasks/cycles/cycles.js
+++ tasks/cycles/cycles.js
@@ -1,23 +1,3 @@
-/**
- * Write the function that will calculate the amount of discount
- * considering the redemption amount
- * Rules are the following:
- * - 0    - 350:  0%
- * - 351  - 1350: 15%
- * - 1351 - 2700: 30%
- * - 2701 - 6500: 45%
- * @param {number} redemption amount (0 - 9999)
- * @returns {number} discount in percent
- */
-
-function calculateDiscount(redemption) {
-  let discount;
-
-  //PLACE YOUR CODE HERE
-
-  return discount;
-}
-
 /** TODO
  * implement factorial algorithm using for, while, do..while operators
  */
