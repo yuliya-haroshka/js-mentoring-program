47ff2d852991638c581578f1676bb620f33813d8
Oleksandr_Halichenko <oleksandr_halichenko@epam.com>
2021-01-25 15:52:55 +0300

    task3 cycles rework

--- tasks/cycles/cycles.js
+++ tasks/cycles/cycles.js
@@ -1,27 +1,39 @@
 /** TODO
  * implement factorial algorithm using for, while, do..while operators
+ * assign result to corresponding variable
+ * for - forFactorial
+ * while - whileFactorial
+ * do..while - doFactorial
  */
-{
-  const i = 10; //10! = 3628800
-}
+let forFactorial, whileFactorial, doFactorial;
+const n = 10; //10! = 3628800
 
-/**
+/** TODO
  * return concatenated string from an array of substring
+ * assign result to variable str
  */
-{
-  const substr = ["I", " love", " JS"];
-}
+let str;
+const substr = ["I", " love", " JS"];
 
-/**
+
+
+/** TODO
  * calculate a total of income of certain person
+ * assign result to variable totalIncome
  */
-{
-  const personIncomes = {
-    salary: 1985,
-    rent: -600,
-    interestOnDeposit: 250,
-    otherExpences: -300
-  };
-}
+let totalIncome;
+const personIncomes = {
+  salary: 1985,
+  rent: -600,
+  interestOnDeposit: 250,
+  otherExpences: -300
+};
 
-module.exports = calculateDiscount;
+
+module.exports = {
+  forFactorial,
+  whileFactorial,
+  doFactorial,
+  str,
+  totalIncome
+};
