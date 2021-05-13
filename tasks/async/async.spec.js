27011483e61634991518815a22103dadf21a71b0
Dzmitry Shaplyka <Dzmitry_Shaplyka@epam.com>
2021-05-13 13:52:49 +0300

    async func moved to another file

--- tasks/async/async.spec.js
+++ tasks/async/async.spec.js
@@ -1,33 +1,6 @@
 const { describe, it } = require('mocha');
 const { expect } = require('chai');
-
-const myPromise = state => {
-	return new Promise((resolve, reject) => {
-		setTimeout(() => {
-			if (state) {
-				resolve('I am resolved');
-			} else {
-				reject('I am rejected');
-			}
-		}, 500);
-	});
-};
-
-const promise1 = () => {
-	return new Promise((resolve, reject) => {
-		setTimeout(() => {
-			resolve('promise1');
-		}, 500);
-	});
-};
-
-const promise2 = () => {
-	return new Promise((resolve, reject) => {
-		setTimeout(() => {
-			resolve('promise2');
-		}, 200);
-	});
-};
+const { myPromise, promise1, promise2 } = require('./async-api');
 
 describe('Promises', () => {
 
